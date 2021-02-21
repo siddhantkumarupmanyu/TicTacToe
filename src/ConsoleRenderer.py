bcdb4e5a12cf612a4f010ef2804903c051392311
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2021-02-21 23:45:34 +0530

    fix rendering bug

--- src/ConsoleRenderer.py
+++ src/ConsoleRenderer.py
@@ -1,5 +1,5 @@
 import os
-from typing import Tuple
+from typing import Tuple, List
 
 from Board import Board, Mark
 from ConsoleInput import ConsoleInput
@@ -15,20 +15,35 @@ class ConsoleRenderer(Renderer):
         self._displayBoard(board)
         self._askNextMove(nextMove)
 
-    def _displayBoard(self, board):
+    def _displayBoard(self, board, winningCells=None):
         self._clearScreen()
-        self._displayLine(board.getValueAt(0, 0), board.getValueAt(0, 1), board.getValueAt(0, 2))
-        self._displayHorizontalLine()
-        self._displayLine(board.getValueAt(1, 0), board.getValueAt(1, 1), board.getValueAt(1, 2))
-        self._displayHorizontalLine()
-        self._displayLine(board.getValueAt(2, 0), board.getValueAt(2, 1), board.getValueAt(2, 2))
+        for i in range(0, 3):
+            winningCellsInRow = self._getWinningCellsInRow(i, winningCells)
+            self._displayLine(board.getValueAt(i, 0), board.getValueAt(i, 1), board.getValueAt(i, 2), winningCellsInRow)
+            if i < 2:
+                self._displayHorizontalLine()
+
+    def _getWinningCellsInRow(self, row, winningCells) -> List:
+        positions = list()
+        if winningCells is None:
+            positions.append(False)
+            positions.append(False)
+            positions.append(False)
+            return positions
+        for i in range(0, 3):
+            if ((row, i) == winningCells[0]) or ((row, i) == winningCells[1]) or ((row, i) == winningCells[2]):
+                positions.append(True)
+            else:
+                positions.append(False)
+        return positions
 
     def displayWinner(self, board: Board, player: Player):
-        self._displayBoard(board)
+        self._displayBoard(board, board.getWinningCells())
         print(f"Winner Is: {self._getIconFromMark(player.getMark())}")
         self._consoleInput.stop()
 
     def tie(self, board: Board, player1: Player, player2: Player):
+        print("\n")
         print(
             f"Its a TIE between {self._getIconFromMark(player1.getMark())} and {self._getIconFromMark(player2.getMark())}"
         )
@@ -37,8 +52,13 @@ class ConsoleRenderer(Renderer):
     def invalidMove(self, player: Player, board: Board, move: Tuple[int, int]):
         print("Invalid Move\n")
 
-    def _displayLine(self, a, b, c):
-        print(f"{self._getIconFromMark(a)} | {self._getIconFromMark(b)} | {self._getIconFromMark(c)}")
+    def _displayLine(self, a, b, c, winningCellsInLine: List):
+        aMark = self._getIconFromMark(a)
+        bMark = self._getIconFromMark(b)
+        cMark = self._getIconFromMark(c)
+        print(
+            f"{self._formatWinningCell(aMark, winningCellsInLine[0])} | {self._formatWinningCell(bMark, winningCellsInLine[1])} | {self._formatWinningCell(cMark, winningCellsInLine[2])}"
+        )
 
     def _clearScreen(self):
         print(end='\n')
@@ -57,3 +77,8 @@ class ConsoleRenderer(Renderer):
             return "O"
         else:
             return " "
+
+    def _formatWinningCell(self, aMark, value):
+        if value:
+            return f"'{aMark}'"
+        return aMark
